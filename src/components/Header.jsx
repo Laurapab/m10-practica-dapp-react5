c44fe1eec796590189b84c9ed74e58f9242507a2
Laurapab <l.palau@hotmail.com>
2024-01-30 10:49:40 +0100

    añadir blockmaker big logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,9 @@
 export default function Header(){
-    return <header>Esto es el header</header>
+    return (
+        <header>
+        <img src="/blockmaker-big-logo.png" />
+        Esto es el header
+        </header>
+    )
+ 
 }
